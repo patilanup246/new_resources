f3d34120851ea47a70cc76af337c474c6f504aed
huguangjing <huguangjing@globalegrow.com>
2018-12-22 09:49:07 +0800

    gengxin

--- spidertest/pythontest.py
+++ spidertest/pythontest.py
@@ -1,64 +1,137 @@
-# _*_ coding:utf-8 _*_
-import csv
-
+import os
 import sys
+from googletrans import Translator
+import traceback
 
-import requests
-from fake_useragent import UserAgent
+countrys = {
+    'af': '南非',
+    'sq': '阿尔巴尼亚',
+    'am': '阿姆哈拉',
+    'ar': '阿拉伯',
+    'hy': '亚美尼亚',
+    'az': '阿塞拜疆',
+    'eu': '巴斯克',
+    'be': '白俄罗斯',
+    'bn': '孟加拉',
+    'bs': '波斯尼亚',
+    'bg': '保加利亚',
+    'ca': '加泰罗尼亚',
+    'ceb': '宿务',
+    'zh-CN': '中国',
+    'zh-TW': '台湾',
+    'co': '科西嘉',
+    'hr': '克罗地亚',
+    'cs': '捷克',
+    'da': '丹麦',
+    'nl': '荷兰',
+    'en': '英国',
+    'eo': '世界',
+    'et': '爱沙尼亚',
+    'fi': '芬兰',
+    'fr': '法国',
+    'fy': '弗里斯兰',
+    'gl': '加利西亚',
+    'ka': '格鲁吉亚',
+    'de': '德国',
+    'el': '希腊',
+    'gu': '古吉拉特',
+    'ht': '海地克里奥尔',
+    'ha': '豪萨',
+    'haw': '夏威夷',
+    'iw': '希伯来',
+    'hi': '印地',
+    'hmn': '苗国',
+    'hu': '匈牙利',
+    'is': '冰岛',
+    'ig': '伊博',
+    'id': '印度尼西亚',
+    'ga': '爱尔兰',
+    'it': '意大利',
+    'ja': '日本',
+    'jw': '爪哇',
+    'kn': '卡纳达',
+    'kk': '哈萨克',
+    'km': '高棉',
+    'ko': '韩国',
+    'ku': '库尔德',
+    'ky': '吉尔吉斯',
+    'lo': '老挝',
+    'la': '拉丁',
+    'lv': '拉脱维亚',
+    'lt': '立陶宛',
+    'lb': '卢森堡',
+    'mk': '马其顿',
+    'mg': '马尔加什',
+    'ms': '马来',
+    'ml': '马拉雅拉姆',
+    'mt': '马耳他',
+    'mi': '毛利',
+    'mr': '马拉地',
+    'mn': '蒙古',
+    'my': '缅甸',
+    'ne': '尼泊尔',
+    'no': '挪威',
+    'ny': '尼杨扎',
+    'ps': '普什图',
+    'fa': '波斯',
+    'pl': '波兰',
+    'pt': '葡萄牙',
+    'pa': '旁遮普',
+    'ro': '罗马尼亚',
+    'ru': '俄罗斯',
+    'sm': '萨摩亚',
+    'gd': '苏格兰盖尔',
+    'sr': '塞尔维亚',
+    'st': '塞索托',
+    'sn': '修纳',
+    'sd': '信德',
+    'si': '僧伽罗',
+    'sk': '斯洛伐克',
+    'sl': '斯洛文尼亚',
+    'so': '索马里',
+    'es': '西班牙',
+    'su': '巽他',
+    'sw': '斯瓦希里',
+    'sv': '瑞典',
+    'tl': '菲律宾',
+    'tg': '塔吉克',
+    'ta': '泰米尔',
+    'te': '泰卢固',
+    'th': '泰国',
+    'tr': '土耳其',
+    'uk': '乌克兰',
+    'ur': '乌尔都',
+    'uz': '乌兹别克',
+    'vi': '越南',
+    'cy': '威尔士',
+    'xh': '班图',
+    'yi': '意第绪',
+    'yo': '约鲁巴',
+    'zu': '祖鲁',
+}
+transd = Translator()
 
-sys.path.append("./..")
-from logs.loggerDefine import loggerDefine
 
-loggerFile = "log.log"
-logging = loggerDefine(loggerFile)
-from db.mongodb import connectMongo
-import jsonpath
-from tools.translate.translate_google import mainTranslate
-from multiprocessing.pool import ThreadPool
-
-db = connectMongo(True)
-collection = db["resources"]
-import json
-import logging
+def checkcountry(describtion, upname):
+    if len(describtion) > 3:
+        rez = transd.detect(describtion).lang
+        country = countrys[rez]
+    else:
+        rez = transd.detect(upname).lang
+        country = countrys[rez]
+    return country
 
 
-def sendRequest(url):
-    for i in range(3):
-        try:
-            headers = {
-                "accept-language": "zh-CN,zh;q=0.9",
-                "user-agent": UserAgent().random,
-                "x-youtube-client-name": "1",
-                "x-youtube-client-version": "2.20181204",
-            }
-            response = requests.get(url=url, headers=headers, timeout=5, verify=False)
-            response.encoding = "utf-8"
-            if response.status_code == 200:
-                return response.text
-        except Exception as e:
-            if repr(e).find("timed out") > 0:
-                logging.error("请求超时{}次,url:{}".format(i + 1, videoUrl))
-            else:
-                logging.error(e)
-
-
-def main_process(result):
-    url = result["url"]
-    videotitleUn = result["videotitleUn"]
-    videoTittle = mainTranslate(videotitleUn)
-    collection.update({"url": url}, {"$set": {"videoTittle": videoTittle}},multi=True)
-    logging.info(url)
+def sendReq(describtion, upname):
+    try:
+        country = checkcountry(describtion, upname)
+        print(country)
+    except Exception as e:
+        print(traceback.format_exc())
 
 
 def readMongo():
-    reslitList = list(
-        collection.find({"videoTittle": "", "videotitleUn": {"$ne": ""}}))
-    for result in reslitList:
-        main_process(result)
-    # pool = ThreadPool(5)
-    # pool.map_async(main_process, reslitList)
-    # pool.close()
-    # pool.join()
+    pass
 
 
 if __name__ == '__main__':
--- spidertest/spidertest.py
+++ spidertest/spidertest.py
@@ -1,68 +1,43 @@
-import sys
-
-sys.path.append("./..")
-from logs.loggerDefine import loggerDefine
-from multiprocessing.pool import ThreadPool
-from db.mongodb import connectMongo
-import time
+import requests
 import json
+from lxml import etree
 
-loggerFile = "backlog.log"
-logging = loggerDefine(loggerFile)
-db = connectMongo(True)
-webResourcesCollection = db["webResources"]
-import re
+url = "https://www.amazon.com/hz/reviews-render/ajax/medley-filtered-reviews/get/ref=cm_cr_dp_d_fltr"
 
+data = {
+    "asin": "B07FVX7Y5J",
+    "sortBy": "helpful",
+    "scope": "reviewsAjax5"
+}
+response = requests.post(url=url, data=data)
 
-def main_process(result):
-    part = result["part"]
-    url = result["url"]
-    print(url)
-    webResourcesCollection.remove({"url": url})
-    try:
-        emailList = list(set(result["connect"].split("\n")))
-    except Exception as e:
-        return
-    for num, email in enumerate(emailList):
-        result["_id"] = "3_" + part + "_" + url + "_" + str(num + 1)
-        # [status, email, firstName, lastName, sourcePage, position]
-        # status:notVerified,email:kerry@bikebling.com,firstName: ,lastName: ,sourcePage: ,position:
-        status = re.search("status:(.*?),", email).group(1).strip()
-        emailStr = re.search("email:(.*?),", email).group(1).strip()
-        firstName = re.search("firstName:(.*?),", email).group(1).strip()
-        lastName = re.search("lastName:(.*?),", email).group(1).strip()
-        sourcePage = re.search("sourcePage:(.*?),", email).group(1).strip()
-        position = re.search("position:(.*?)", email).group(1).strip()
-        print(
-            "status:{},email:{},firstName:{},lastName:{},sourcePage:{},position:{}".format(status, emailStr, firstName,
-                                                                                           lastName, sourcePage,
-                                                                                           position))
+reslist = response.text.replace("&&&", "").split("\n")
+responseStr = ""
+for i in reslist:
+    if not i:
+        continue
+    # print(i.strip())
+    responseStr += json.loads(i.strip())[-1]
 
-        if not emailStr:
-            continue
-        result["emailStr"] = emailStr
-        result["status"] = status
-        result["firstName"] = firstName
-        result["lastName"] = lastName
-        result["sourcePage"] = sourcePage
-        result["position"] = position
-        result["isGetLink"] = True
-        result["connect"] = ""
-        try:
-            webResourcesCollection.insert(result)
-        except Exception as e:
-            logging.error(e)
+# print(responseStr)
 
+selector = etree.HTML(responseStr)
+nodeList = selector.xpath('//div[@data-hook="review"]')
+for node in nodeList:
+    # 用户
+    print(node.xpath('.//a[@data-hook="review-author"]/text()')[0])
+    # 评分
+    print(node.xpath('.//div[@class="a-row"]/a[1]/@title')[0])
+    # 评论标题
+    print(node.xpath('.//div[@class="a-row"]/a[2]/text()')[0])
+    # 评论时间
+    print(node.xpath('.//span[@data-hook="review-date"]/text()')[0])
+    #  Verified Purchase
+    print(node.xpath('.//span[@data-hook="avp-badge-linkless"]/text()')[0])
+    # 评论内容
+    print(node.xpath('.//span[@data-hook="review-body-truncated"]/text()')[0])
+    # 图片链接
+    print(node.xpath('.//div[@class="review-image-tile-section "]//img/@src'))
+    # 视频
+    print(node.xpath('.//input/@value'))
 
-def readMongo():
-    resultList = list(webResourcesCollection.distinct("emailStr",{"emailStr": {"$ne": ""}}))
-    for result in resultList:
-
-        mail = result.split("\n")
-
-        if len(mail) != 1:
-            print(1)
-
-
-if __name__ == '__main__':
-    readMongo()
